360397b49c43fa1328ef39b6e1e8225a87b088da
Victor Hugo Maciel de Lima <victorh5.empresa@gmail.com>
2023-06-01 09:38:09 -0400

    feat: creating custom button component.

---
+++ components/sas-button.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-button
+      class="normal"
+      :class="[{'outlined': outlined}]"
+    >
+      <slot />
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    outlined: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.normal {
+  background-color: #FB3B11;
+  border-color: #FB3B11;
+}
+.outlined {
+  background-color: transparent;
+  border-color: #FB3B11;
+  color: #FB3B11;
+}
+</style>
