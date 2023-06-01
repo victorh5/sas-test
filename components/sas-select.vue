5a00fac884197de0159acde37c7f4026ebd6c550
Victor Hugo Maciel de Lima <victorh5.empresa@gmail.com>
2023-06-01 15:29:35 -0400

    feat: select component.

---
+++ components/sas-select.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="select">
+    <label :for="name">{{ label }}</label>
+    <select :id="name" v-model="model">
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: [String, Number],
+      default: undefined
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    options: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    name () {
+      return this.label.toLowerCase()
+    },
+    model: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.select {
+  display: grid;
+}
+.select select {
+  padding: 0.50rem;
+  border-radius: 0.25rem;
+  border: 1px solid black;
+}
+</style>
