7722f37711cbc1e69c231d84631d12aa2267952c
Victor Hugo Maciel de Lima <victorh5.empresa@gmail.com>
2023-05-31 23:50:20 -0400

    feat: adding token information

--- store/user.js
+++ store/user.js
@@ -1,12 +1,15 @@
 export const state = () => ({
-  information: {}
+  information: {},
+  token: ''
 })
 
 export const mutations = {
   login (state, data) {
     state.information = data
+    state.token = 'Bearer 40fe071962846075452a4f6123ae71697463cad20f51e237e2035b41af0513d8'
   },
   logout (state) {
     state.information = {}
+    state.token = ''
   }
 }
