f7e7ee62092a3d260dc9fe75bcbd8f858439d685
Victor Hugo Maciel de Lima <victorh5.empresa@gmail.com>
2023-05-31 23:48:30 -0400

    feat: adding nuxt vuex persist.

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/vuex-persist', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -47,7 +48,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
+    baseURL: 'https://sys-dev.searchandstay.com/api/admin'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
