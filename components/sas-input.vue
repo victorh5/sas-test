aa0b9c237df5580f8ddc297e1e308dbb5f2f29a9
Victor Hugo Maciel de Lima <victorh5.empresa@gmail.com>
2023-05-31 23:11:46 -0400

    feat: creating custom input component.

---
+++ components/sas-input.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <label for="inline-form-input-name">{{ label }}</label>
+    <b-form-input
+      :id="id"
+      :value="modelValue"
+      :type="type"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      @input="$emit('input', $event)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    }
+  },
+  computed: {
+    id () {
+      return this.label.toLowerCase()
+    }
+  },
+  methods: {
+    updateValue (event) {
+      this.$emit('update:modelValue', event)
+    }
+  }
+}
+</script>
