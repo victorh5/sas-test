f3aee675a5d16a253b65be767416773e98a65273
Victor Hugo Maciel de Lima <victorh5.empresa@gmail.com>
2023-06-01 03:47:22 -0400

    feat: adding table with house rules data, pagination configured!

--- pages/house-rules.vue
+++ pages/house-rules.vue
@@ -1,24 +1,71 @@
 <template>
   <div>
-    LOGADO
-    {{ $store.state.user.token }}
+    <b-table
+      striped
+      :items="houseRules.entities"
+      :fields="tableFields"
+      :per-page="houseRules?.pagination?.per_page"
+      fixed
+      outlined
+      :busy="isBusy"
+    >
+      <template #table-busy>
+        <div class="text-center my-2">
+          <b-spinner class="align-middle" />
+          <strong>Loading...</strong>
+        </div>
+      </template>
+      <template #cell(actions)="data">
+        <b-button @click="handleEdit(data.item.id)">
+          Edit
+        </b-button>
+        <b-button @click="handleRemove(data.item.id)">
+          Remove
+        </b-button>
+      </template>
+    </b-table>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="houseRules?.pagination?.total"
+      :per-page="houseRules?.pagination?.per_page"
+    />
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
+    tableFields: [
+      { key: 'id', sortable: true },
+      { key: 'name', sortable: true },
+      { key: 'active', sortable: true, formatter: value => value === 1 ? 'Ativo' : 'Inativo' },
+      { key: 'actions' }
+    ],
+    houseRules: {},
+    currentPage: 1,
+    isBusy: false
   }),
+  watch: {
+    currentPage () {
+      this.loadHouseRules()
+    }
+  },
   async mounted () {
     await this.loadHouseRules()
   },
   methods: {
     async loadHouseRules () {
-      await this.$axios.$get('/house_rules', {
+      this.isBusy = true
+      const { data } = await this.$axios.$get('/house_rules', {
+        params: {
+          page: this.currentPage
+        },
         headers: {
           Authorization: this.$store.state.user.token
         }
       })
+      this.houseRules = data
+      this.isBusy = false
     }
   }
 }
